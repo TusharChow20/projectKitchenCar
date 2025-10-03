ad8e0f8507369e99253e8fb289962a5acb57db06
Tushar Chowdhury <tusharchowdhury20211@gmail.com>
2025-10-03 17:25:02 +0600

    trac order

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -3,7 +3,7 @@ import React from "react";
 // import img from "../assets/title-bg.png";
 const Banner = ({ children }) => {
   return (
-    <div className="bg-[url('/title-bg.png')] hidden  md:flex items-center justify-center gap-4 bg-no-repeat pb-45 pt-30 bg-center">
+    <div className="bg-[url('/title-bg.png')] hidden  md:flex items-center justify-center gap-4 bg-no-repeat bg-cover pb-20 pt-14 lg:pb-45 lg:pt-30 bg-center">
       <ChefHat size={50} color="black" />
       <h1
         className="text-center  text-black text-bold font-bold text-5xl"
---
+++ src/Components/TrackOrder.jsx
@@ -0,0 +1,36 @@
+import { CookingPot, ScrollText, TicketsPlane } from "lucide-react";
+import React from "react";
+{
+  /*
+   */
+}
+const TrackOrder = () => {
+  return (
+    <div className="grid md:grid-cols-3 gap-5 mt-10 px-5">
+      {/* <h1>Trac order</h1> */}
+      <div className="flex items-center justify-between border-4 border-dotted border-amber-300 rounded-xl  p-10">
+        <ScrollText size={50} color="#fcb900" />
+        <div className="text-center">
+          <h1 className="text-2xl">Current Orders</h1>
+          <p className="font-bold text-4xl">{0}</p>
+        </div>
+      </div>
+      <div className="flex items-center justify-between border-4 border-dotted border-amber-300 rounded-xl  p-10">
+        <CookingPot size={50} color="#fcb900" />
+        <div className="text-center">
+          <h1 className="text-2xl">Currently Cooking</h1>
+          <p className="font-bold text-4xl">{0}</p>
+        </div>
+      </div>
+      <div className="flex items-center justify-between border-4 border-dotted border-amber-300 rounded-xl  p-10">
+        <TicketsPlane size={50} color="#fcb900" />
+        <div className="text-center">
+          <h1 className="text-2xl">Ready to Serve</h1>
+          <p className="font-bold text-4xl">{0}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TrackOrder;
